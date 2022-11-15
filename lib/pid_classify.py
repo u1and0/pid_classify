755732b8bb3c3b8cb279b089382bb89aeb014921
u1and0 <e01.ando60@gmail.com>
2022-11-15 14:07:32 +0900

    [doc] docstring

--- lib/pid_classify.py
+++ lib/pid_classify.py
@@ -8,10 +8,12 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
+# PidMaster : 学習データ(品名、型式)、教師データ(品番カテゴリ)のデータコンテナ
 PidMaster = namedtuple("PidMaster", ["names", "models", "categories"])
 
 
 def load_data(datapath: str) -> PidMaster:
+    """csvファイルを読み込んで品番マスタデータを返す"""
     cwd = os.path.dirname(__file__)
     df = pd.read_csv(cwd + "/" + datapath)
     pid = df["品番"].values
@@ -23,6 +25,7 @@ def load_data(datapath: str) -> PidMaster:
 
 @dataclass
 class PidClassify:
+    """品番カテゴリ分類器クラス"""
     clf: MultinomialNB
     vectorizer: HashingVectorizer
     le: LabelEncoder
@@ -32,9 +35,8 @@ class PidClassify:
     y_test: list
 
     def predict(self, name, model: str) -> str:
-        """
-        適当な品名 name を入れて、
-        学習機により提案された品番カテゴリを一つ返す
+        """ 品名と型式の組から、
+        学習器により提案された品番カテゴリを一つ返す
         """
         namemodel = f"{name}\t{model}"
         vec = self.vectorizer.fit_transform([namemodel])
@@ -50,9 +52,6 @@ class PidClassify:
         index = self.le.inverse_transform(self.clf.classes_)
         se = pd.Series(prob[0], index).sort_values(ascending=False)
         return se[:top]
-
-    def score(self):
-        return self.clf.score(self.X_test, self.y_test)
 
     def predict_mask_proba(self,
                            name: str,
@@ -73,8 +72,13 @@ class PidClassify:
             predict_list.append(pid)
         return predict_list
 
+    def score(self):
+        """テストデータによるスコア算出"""
+        return self.clf.score(self.X_test, self.y_test)
 
-def training(pid_master: PidMaster):
+
+def training(pid_master: PidMaster) -> PidClassify:
+    """品番データを学習して学習器を生成する"""
     # テキストをtrigram特徴量に変換
     vectorizer = HashingVectorizer(
         n_features=2**16,
@@ -82,22 +86,17 @@ def training(pid_master: PidMaster):
         ngram_range=(3, 3),  # trigram
         binary=True,
         norm=None)
-
     # 品名 / 型式をタブ区切り
     X = vectorizer.fit_transform(
         f"{n}\t{m}" for n, m in zip(pid_master.names, pid_master.models))
-
     # カテゴリ文字列を数値に変換
     le = LabelEncoder()
     y = le.fit_transform(pid_master.categories)
-
     # データをトレーニング用、テスト用に分割します。
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
     # ナイーブベイズにより学習させます。
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
-
     return PidClassify(clf, vectorizer, le, X_train, X_test, y_train, y_test)
 
 
