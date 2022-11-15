002e01b6d2f33f3c43c1ce1bfa920afd4f287c41
u1and0 <e01.ando60@gmail.com>
2022-11-15 14:01:05 +0900

    [mod] rename for useablily as libraly

--- lib/pid_classify.py
+++ lib/pid_classify.py
@@ -15,10 +15,10 @@ def load_data(datapath: str) -> PidMaster:
     cwd = os.path.dirname(__file__)
     df = pd.read_csv(cwd + "/" + datapath)
     pid = df["品番"].values
-    name = df["品名"].values
-    types = df["型式"].values
-    pid_category = [i.split("-")[0] for i in pid]
-    return PidMaster(name, types, pid_category)
+    names = df["品名"].values
+    models = df["型式"].values
+    categories = [i.split("-")[0] for i in pid]
+    return PidMaster(names, models, categories)
 
 
 @dataclass
@@ -26,44 +26,43 @@ class PidClassify:
     clf: MultinomialNB
     vectorizer: HashingVectorizer
     le: LabelEncoder
+    X_train: list
+    X_test: list
+    y_train: list
+    y_test: list
 
-    def predict_pid(self, clf: MultinomialNB, vectorizer: HashingVectorizer,
-                    le: LabelEncoder, name: str) -> str:
+    def predict(self, name, model: str) -> str:
         """
         適当な品名 name を入れて、
         学習機により提案された品番カテゴリを一つ返す
-        学習済みの学習機clfを引数に取る。
         """
-        vec = self.vectorizer.fit_transform([name])
+        namemodel = f"{name}\t{model}"
+        vec = self.vectorizer.fit_transform([namemodel])
         category_idx = self.clf.predict(vec)
         predict_pid = self.le.inverse_transform(category_idx)
         return predict_pid[0]
 
-    # MultinominalNBクラスのメソッドとしてpredict_pidを登録
-    # clf.predict_pid(name)として実行できる
-    # setattr(MultinomialNB, "predict_pid", predict_pid)
-
-    def prob_series(self,
-                    clf: MultinomialNB,
-                    vectorizer: HashingVectorizer,
-                    le: LabelEncoder,
-                    name: str,
-                    top: int = 5) -> pd.Series:
-        """適当な品名nameが属する品番カテゴリの上位 top件を返す"""
-        vec = self.vectorizer.fit_transform([name])
+    def predict_proba(self, name, model: str, top: int = 5) -> pd.Series:
+        """品名と型式の組から想定される品番カテゴリの上位 top件を返す"""
+        namemodel = f"{name}\t{model}"
+        vec = self.vectorizer.fit_transform([namemodel])
         prob = self.clf.predict_proba(vec)
         index = self.le.inverse_transform(self.clf.classes_)
         se = pd.Series(prob[0], index).sort_values(ascending=False)
         return se[:top]
 
-    # MultinominalNBクラスのメソッドとしてpredict_pidを登録
-    # clf.predict_pid(name)として実行できる
-    # setattr(MultinomialNB, "prob_series", prob_series)
+    def score(self):
+        return self.clf.score(self.X_test, self.y_test)
 
-    @staticmethod
-    def pid_mask_probability(pid_series: pd.Series,
-                             threshold=0.95) -> list[str]:
-        """品番のサジェスト確率の累計がthresholdを超えるまでのリストを返す"""
+    def predict_mask_proba(self,
+                           name: str,
+                           model: str,
+                           top=100,
+                           threshold=0.95) -> list[str]:
+        """ 品名と型式の組から想定される品番カテゴリを複数返す。
+        品番のサジェスト確率の累計がthresholdを超えるまでのリストを返す
+        """
+        pid_series = self.predict_proba(name, model, top)
         se_iter = pid_series.iteritems()
         predict_list = []
         cumsum_percentile = 0
@@ -99,29 +98,29 @@ def training(pid_master: PidMaster):
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
 
-    # 学習の評価
-    score = clf.score(X_test, y_test)
-    assert 0.60 < score < 0.99, "適切な精度で学習できていません。"
-    print(f"学習精度 {score:.4}で学習を完了しました。")
-    return PidClassify(clf, vectorizer, le)
+    return PidClassify(clf, vectorizer, le, X_train, X_test, y_train, y_test)
 
 
-if __name__ == "__main__":
-    master = load_data("../data/pidmaster.csv")
-    classifier = training(master)
+### main ###
+master = load_data("../data/pidmaster.csv")
+classifier = training(master)
+# 学習の評価
+score = classifier.score()
+assert 0.60 < score < 0.99, "適切な精度で学習できていません。"
+print(f"学習精度 {score:.4}で学習を完了しました。")
 
-    # Usage
-    #
-    # se = clf.prob_series(vectorizer, le, name="ブレーカ\tBBW351", top=100)
-    # print(se)
-    # pid_mask_probability(se)
-    #
-    #
-    # 同じインターフェースで、上位5件を順位付きで表示しました。
-    # 確率が分散しているので、上位5件を超えたところで閾値を超えためです。
-    #
-    # 順位すらいらない、不要な情報だ、ということであれば、listをsetにすればいいだけです。
-    #
-    #
-    # predict_set = set(pid_mask_probability(se))
-    # print(f"品名: ブレーカ 型式:BBW351 の品番は95%以上の確率で{predict_set}のいずれかです。")
+# Usage
+#
+# se = clf.predict_proba(vectorizer, le, name="ブレーカ\tBBW351", top=100)
+# print(se)
+# predict_noproba(se)
+#
+#
+# 同じインターフェースで、上位5件を順位付きで表示しました。
+# 確率が分散しているので、上位5件を超えたところで閾値を超えためです。
+#
+# 順位すらいらない、不要な情報だ、ということであれば、listをsetにすればいいだけです。
+#
+#
+# predict_set = set(predict_noproba(se))
+# print(f"品名: ブレーカ 型式:BBW351 の品番は95%以上の確率で{predict_set}のいずれかです。")
